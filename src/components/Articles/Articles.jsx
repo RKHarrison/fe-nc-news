de854a03414570ac3fbb7ba74dc498ae8b894ac4
Richard Harrison <r.harr.k@gmail.com>
2024-06-17 20:03:33 +0100

    adds extra information and formatting to each article card

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -5,30 +5,38 @@ import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     getArticles().then((articlesFromApi) => {
       setArticles(articlesFromApi);
-      setIsLoading(false)
+      setIsLoading(false);
     });
   }, []);
 
   return (
     <section className="articles-section">
-      {isLoading && <LoadingSpinner/>}
+      {isLoading && <LoadingSpinner />}
       <ol>
         {articles[0] && (
           <li key={articles[0].article_id} className="main-article">
             <img src={articles[0].article_img_url} />
             <h2>{articles[0].title}</h2>
+            <h4>Created by {articles[0].author} on {articles[0].created_at.slice(0, 10)}</h4>
+            <p>Topic: {articles[0].topic}</p>
+              <p>Votes: {articles[0].votes}</p>
           </li>
         )}
         {articles.slice(1).map((article) => (
           <li key={article.article_id} className="norm-article">
-            <img src={article.article_img_url} />
-            <h2>{article.title}</h2>
+              <img src={article.article_img_url} />
+              <h3> {article.title}</h3>
+              <h4>
+                Created by {article.author} on {article.created_at.slice(0, 10)}
+              </h4>
+              <p>Topic: {article.topic}</p>
+              <p>Votes: {article.votes}</p>
           </li>
         ))}
       </ol>
