a6313891ad9f76556da15717d34af283039b3f58
Richard Harrison <r.harr.k@gmail.com>
2024-06-18 10:38:03 +0100

    extracts reusable ArticleCard component

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -3,12 +3,12 @@ import { getArticleById, getArticles } from "../../utils/api";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+import ArticlesCard from "../ArticlesCard/ArticlesCard";
 
 const Articles = ({ setArticle }) => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
-
 
   useEffect(() => {
     setIsLoading(true);
@@ -39,14 +39,7 @@ const Articles = ({ setArticle }) => {
               key={articles[0].article_id}
               className="main-article"
             >
-              <img src={articles[0].article_img_url} />
-              <h2> {articles[0].title}</h2>
-              <h3>
-                Created by {articles[0].author} on
-                {articles[0].created_at.slice(0, 10)}
-              </h3>
-              <p>Topic: {articles[0].topic}</p>
-              <p>Votes: {articles[0].votes}</p>
+              <ArticlesCard article={articles[0]} />
             </li>
           )}
           {articles.slice(1).map((article) => (
@@ -55,13 +48,7 @@ const Articles = ({ setArticle }) => {
               key={article.article_id}
               className="norm-article"
             >
-              <img src={article.article_img_url} />
-              <h2> {article.title}</h2>
-              <h3>
-                Created by {article.author} on {article.created_at.slice(0, 10)}
-              </h3>
-              <p>Topic: {article.topic}</p>
-              <p>Votes: {article.votes}</p>
+              <ArticlesCard article={article} />
             </li>
           ))}
         </ol>
