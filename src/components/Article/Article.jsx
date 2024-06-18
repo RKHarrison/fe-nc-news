21bfb4b53849ae5f68f660012087b1c9ce3ce0c2
Richard Harrison <r.harr.k@gmail.com>
2024-06-18 12:27:30 +0100

    renders Comments in parent component and props article.article_id from parent

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,10 +1,12 @@
 import "./Article.css";
 import ArticleCard from "../ArticleCard/ArticleCard";
+import Comments from "../Comments/Comments";
 
 const Article = ({ article }) => {
   return (
     <section className="article-section">
       {article.title && <ArticleCard article={article} />}
+      <Comments article_id={article.article_id} />
     </section>
   );
 };
