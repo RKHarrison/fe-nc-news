60c4b2077704eee38aa2d3764b8bd7048306bfcf
Richard Harrison <r.harr.k@gmail.com>
2024-06-18 13:33:20 +0100

    adds loading behaviour to comments

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -2,28 +2,32 @@ import { useEffect, useState } from "react";
 import { getCommentsByArticleId } from "../../utils/api";
 import "./Comments.css"
 import CommentCard from "../CommentCard/CommentCard";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 const Comments = ({ article_id }) => {
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
-    if (article_id) {
+    article_id && 
+      setIsLoading(true)
       getCommentsByArticleId(article_id).then((commentsFromApi) => {
         setComments(commentsFromApi);
+        setIsLoading(false)
       });
-    }
   }, [article_id]);
 
   return (
     <article className="comments-section">
       <h2>Comments...</h2>
+      {isLoading? ( <LoadingSpinner/>) : (
       <ol className="comment-list">
         {comments.map((comment) => (
           <li key={comment.comment_id}>
             <CommentCard comment={comment}/>
           </li>
         ))}
-      </ol>
+      </ol>)}
     </article>
   );
 };
