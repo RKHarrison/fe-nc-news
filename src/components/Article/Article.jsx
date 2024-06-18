4fdd4e1a2119d672eb7fc22c2b62c11d231fda16
Richard Harrison <r.harr.k@gmail.com>
2024-06-18 09:19:44 +0100

    Adds Article component to render user-selected article

---
+++ src/components/Article/Article.jsx
@@ -0,0 +1,22 @@
+import "./Article.css";
+
+const Article = ({ article }) => {
+  return (
+    <section className="article-section">
+      {article.title && (
+        <article>
+          <img src={article.article_img_url} />
+          <h2> {article.title}</h2>
+          <h3>
+            Created by {article.author} on {article.created_at.slice(0,10)}
+          </h3>
+          <p>Topic: {article.topic}</p>
+          <p>Votes: {article.votes}</p>
+          <p>{article.body}</p>
+        </article>
+      )}
+    </section>
+  );
+};
+
+export default Article;
