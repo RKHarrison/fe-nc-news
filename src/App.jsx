191830cd6c800829dd3c1949b9e7c97ec751d243
Richard Harrison <r.harr.k@gmail.com>
2024-06-18 09:20:31 +0100

    adds routes to handle rendering of different views

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import "./App.css";
+import { useState } from "react";
+import {Route, Routes} from "react-router-dom" 
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Articles from "./components/Articles/Articles";
+import Article from "./components/Article/Article";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [article, setArticle] = useState({})
+
   return (
-    <body>
+    <>
       <Header />
       <Nav />
-      <Articles />
-    </body>
+      <Routes>
+        <Route path="/article/:article_id" element={<Article article={article}/>}/>
+        <Route path="/" element={<Articles articles={articles} setArticles={setArticles} setArticle={setArticle}/>}/>
+      </Routes>
+    </>
   );
 }
 
