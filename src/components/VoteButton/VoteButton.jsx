6b8d7293ed555d7a20eb808dbadd41909b5b60c6
Richard Harrison <r.harr.k@gmail.com>
2024-06-19 09:36:38 +0100

    adds error handling to vote button based on bad api request, resetting optimistic rendering of vote and providign error message

--- src/components/VoteButton/VoteButton.jsx
+++ src/components/VoteButton/VoteButton.jsx
@@ -2,24 +2,47 @@ import "./VoteButton.css";
 import { useParams } from "react-router-dom";
 import { patchArticle } from "../../utils/api";
 import { useState } from "react";
+import { FaArrowCircleUp, FaArrowCircleDown } from "react-icons/fa";
 
 const VoteButton = ({ setArticle }) => {
+  const [apiVoteError, setApiVoteError] = useState(false);
+
   const [vote, setVote] = useState(0);
   const { article_id } = useParams();
 
+
+
   const handleVoteClick = (voteButton) => {
+    setApiVoteError(false);
+
+    setVote((currentVote) => currentVote + voteButton);
     setArticle((currentArticle) => ({
       ...currentArticle,
       votes: currentArticle.votes + voteButton,
     }));
-    setVote((currentVote) => currentVote + voteButton);
-    patchArticle(article_id, voteButton);
+    
+    patchArticle(article_id, voteButton).catch(() => {
+      setApiVoteError(true);
+      setVote((currentVote) => currentVote - voteButton);
+      setArticle((currentArticle) => ({
+        ...currentArticle,
+        votes: currentArticle.votes - voteButton,
+      }));
+    });
   };
 
   return (
     <>
-      {vote <= 0 && <button onClick={() => handleVoteClick(1)}>+</button>}
-      {vote >= 0 && <button onClick={() => handleVoteClick(-1)}>-</button>}
+
+
+      {vote >= 0 &&  <FaArrowCircleDown onClick={() => handleVoteClick(-1)}/>}
+      {vote <= 0 &&  <FaArrowCircleUp onClick={() => handleVoteClick(1)}/>}
+      
+      {apiVoteError && (
+        <section className="error-message">
+          Could not register your vote, please try again!
+        </section>
+      )}
     </>
   );
 };
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -39,11 +39,17 @@ export const getCommentsByArticleId = (article_id) => {
 
 export const patchArticle = (article_id, vote) => {
   return ncNewsApi
-  .patch(`articles/${article_id}`, {inc_votes: vote})
-  .then(({data}) => {
-    return data.article
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-}
+    .patch(`articles/${article_id}`, { inc_votes: vote })
+    .then(({ data }) => {
+      return data.patchedArticle;
+    })
+    .catch((err) => {
+      if (err.response?.status === 404) {
+        console.error(
+          `Could not find article with id=${article_id} for vote patch request`
+        );
+      }
+      console.error(err);
+      return Promise.reject(err);
+    });
+};
