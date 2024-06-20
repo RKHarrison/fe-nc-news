2bd1e6285a9f77ac882c10395160e2e456f53aa3
Richard Harrison <r.harr.k@gmail.com>
2024-06-20 15:09:10 +0100

    removes link from header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,12 @@
 import UserCard from "../UserCard/UserCard";
 import "./Header.css";
-import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Link to='/' className="header-section">
+    <section className="header-section">
       <h1>NC News</h1>
       <UserCard/>
-    </Link>
+    </section>
   );
 };
 
