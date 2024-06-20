ef2b101cc6f349c0bad0452d0079c01b3e6d3195
Richard Harrison <r.harr.k@gmail.com>
2024-06-20 18:47:37 +0100

    creates SortSelect componenet to handle user query input

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,26 @@
 import "./Nav.css";
 import { Link } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
+import SortSelect from "../SortSelect/SortSelect";
 
 const Nav = () => {
+
   return (
     <nav>
-      <Link className="nav-link" id="home" to="/">Latest News</Link>
-      <Link className="nav-link" to="/coding">Coding</Link>
-      <Link className="nav-link" to="/football">Football</Link>
-      <Link className="nav-link" to="/cooking">Cooking</Link>
+      <Link className="nav-link" id="home" to="/">
+        Latest News
+      </Link>
+      <Link className="nav-link" to="/coding">
+        Coding
+      </Link>
+      <Link className="nav-link" to="/football">
+        Football
+      </Link>
+      <Link className="nav-link" to="/cooking">
+        Cooking
+      </Link>
+      <SortSelect/>
+     
     </nav>
   );
 };
