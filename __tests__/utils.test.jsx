687574180b95e602da957137facd1fe08e992b41
Richard Harrison <r.harr.k@gmail.com>
2024-06-20 11:17:25 +0100

    adds a test folder with test seuite for new util func

---
+++ __tests__/utils.test.jsx
@@ -0,0 +1,34 @@
+const hasTemporaryId = require("../src/components/Utils/component-utils")
+
+describe('hasTemporaryId', () => {
+  it('should return false given a comment without a comment_id', () => {
+    const comment = {};
+    const result = hasTemporaryId(comment)
+    expect(result).toBe(false);
+  });
+  it('should return true if comment_id starts with "tempId"', () => {
+    const comment = { comment_id: 'tempId123' };
+    const result = hasTemporaryId(comment)
+    expect(result).toBe(true);
+  });
+  it('should return false if comment_id does not start with "tempId"', () => {
+    const comment = { comment_id: 'id123tempId' };
+    const result = hasTemporaryId(comment)
+    expect(result).toBe(false);
+  });
+  it('should return false if comment_id is purely numeric', () => {
+    const comment = { comment_id: 123456 };
+    const result = hasTemporaryId(comment)
+    expect(result).toBe(false);
+  });
+  it('should handle comments where comment_id is undefined', () => {
+    const comment = { comment_id: undefined };
+    expect(hasTemporaryId(comment)).toBe(false);
+  });
+  it('should not mutate input object', () => {
+    const comment = { comment_id: 'tempId123' };
+    const commentCopy = { comment_id: 'tempId123' }
+    hasTemporaryId(comment)
+    expect(comment).toEqual(commentCopy)
+  })
+})
