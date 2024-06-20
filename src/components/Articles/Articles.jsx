f70a763279aac281f065dae8205300de32ef2b28
Richard Harrison <r.harr.k@gmail.com>
2024-06-20 18:50:17 +0100

    accesses searchParams in articeles component and passes them to axios get function, adds searchParams as dependencies in useEffect

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -3,51 +3,47 @@ import { getArticles } from "../../utils/api";
 import { useState, useEffect } from "react";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 import ArticlesCard from "../ArticlesCard/ArticlesCard";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const {topic} = useParams()
+  const { topic } = useParams();
+  const [searchParams] = useSearchParams()
 
-
+  
   useEffect(() => {
-    setIsLoading(true)
-    getArticles(topic).then((articlesFromApi) => {
-      console.log(topic);
+    const sort_by = searchParams.get('sort_by')
+    const order = searchParams.get('order')
+    setIsLoading(true);
+    getArticles(topic, sort_by, order).then((articlesFromApi) => {
       setArticles(articlesFromApi);
       setIsLoading(false);
     });
-  }, [topic]);
+  }, [topic, searchParams]);
 
   return (
     <>
-    <section className="articles-section">
-      <div className="grid-wrapper">
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : (
-        <ol>
-          {articles[0] && (
-            <li
-              key={articles[0].article_id}
-              className="main-article"
-            >
-              <ArticlesCard article={articles[0]} />
-            </li>
+      <section className="articles-section">
+        <div className="grid-wrapper">
+          {isLoading ? (
+            <LoadingSpinner />
+          ) : (
+            <ol>
+              {articles[0] && (
+                <li key={articles[0].article_id} className="main-article">
+                  <ArticlesCard article={articles[0]} />
+                </li>
+              )}
+              {articles.slice(1).map((article) => (
+                <li key={article.article_id} className="norm-article">
+                  <ArticlesCard article={article} />
+                </li>
+              ))}
+            </ol>
           )}
-          {articles.slice(1).map((article) => (
-            <li
-              key={article.article_id}
-              className="norm-article"
-            >
-              <ArticlesCard article={article} />
-            </li>
-          ))}
-        </ol>
-      )}
-      </div>
-    </section>
+        </div>
+      </section>
     </>
   );
 };
