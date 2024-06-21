d03e2f8f8f5f2b1d3e6b302c822538b7781d0e02
Richard Harrison <r.harr.k@gmail.com>
2024-06-21 16:09:49 +0100

    refactors formatDateStamp

--- src/components/Utils/component-utils.js
+++ src/components/Utils/component-utils.js
@@ -3,40 +3,15 @@ export function hasTemporaryId(comment) {
 }
 
 export function formatDateStamp(timeStamp) {
-  let errMsg = "Date unavailable, sorry!";
-  if (!timeStamp) return errMsg;
-
-  const date = timeStamp.slice(0, 10).split("-");
-  const time = timeStamp.slice(11, 19).split(":");
-  const year = Number(date[0]);
-  const month = Number(date[1]) - 1;
-  const day = Number(date[2]);
-  const hour = Number(time[0]);
-  const minute = Number(time[1]);
-  const second = Number(time[2]);
-
-  switch (true) {
-    case month < 0 || month > 11:
-      return errMsg;
-    case day < 1 || day > 31:
-      return errMsg;
-    case hour < 0 || hour > 23:
-      return errMsg;
-    case minute < 0 || minute > 59:
-      return errMsg;
-    case second < 0 || second > 59:
-      return errMsg;
-    default:
-  }
-
-  const event = new Date(Date.UTC(year, month, day, hour, minute, second));
+  if (!timeStamp) return "Date unavailable, sorry!";
+  
+  const event = new Date(timeStamp);
   const options = {
     weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
   };
-
   return event.toLocaleDateString("en-GB", options);
 }
 
