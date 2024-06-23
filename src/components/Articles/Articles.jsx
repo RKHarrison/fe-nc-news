0a0fc0292750ad61071d3921f1dd09b8a2ed7a17
Richard Harrison <r.harr.k@gmail.com>
2024-06-23 13:13:04 +0100

    rewords conditional checks for consistency and clarity

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -2,7 +2,7 @@ import "./Articles.css";
 import ErrorComponent from "../ErrorComponent/ErrorComponent";
 import { getArticles } from "../../utils/api";
 import { useState, useEffect } from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams, useSearchParams, Link } from "react-router-dom";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 import ArticlesHeadlinesCard from "../ArticlesHeadlinesCard/ArticlesHeadlinesCard";
 import SortBySelect from "../SortBySelect/SortBySelect";
@@ -33,43 +33,43 @@ const Articles = () => {
 
   if (error) {
     return <ErrorComponent error={error} />;
-  }
-  return (
+  }return (
     <>
-      {articles[0] ? (
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
         <>
-          <section className="sort-by-section">
-            <SortBySelect />
-          </section>
-          {isLoading ? (
-            <LoadingSpinner />
+          {articles.length ? (
+            <>
+              <section className="sort-by-section">
+                <SortBySelect />
+              </section>
+              <section className="articles-section">
+                <div className="grid-wrapper">
+                  <ol>
+                    <li key={articles[0].article_id} className="lead-article">
+                      <ArticlesHeadlinesCard
+                        classname="articles-card"
+                        article={articles[0]}
+                      />
+                    </li>
+                    {articles.slice(1, 13).map((article) => (
+                      <li key={article.article_id} className="headline-article">
+                        <ArticlesHeadlinesCard article={article} />
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+                <Pagination />
+              </section>
+            </>
           ) : (
             <section className="articles-section">
-              <div className="grid-wrapper">
-                <ol>
-                  <li key={articles[0].article_id} className="lead-article">
-                    <ArticlesHeadlinesCard
-                      classname="articles-card"
-                      article={articles[0]}
-                    />
-                  </li>
-
-                  {articles.slice(1, 13).map((article) => (
-                    <li key={article.article_id} className="headline-article">
-                      <ArticlesHeadlinesCard article={article} />
-                    </li>
-                  ))}
-                </ol>
-              </div>
-              <Pagination />
+              <h2>No articles to display!</h2>
+              <Link to='/'>Take me home</Link>
             </section>
           )}
         </>
-      ) : (
-        <section className="articles-section">
-          <h2>No articles to display!</h2>
-          <Pagination />
-        </section>
       )}
     </>
   );
